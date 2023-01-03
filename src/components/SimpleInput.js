a5bdbe75e96a2ee9df54c23ac3db51a5e64dd9b2
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-03 15:56:59 +0200

    adds ref and state input manipulations and submit handler

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,35 @@
+import {useRef, useState} from "react";
+
 const SimpleInput = (props) => {
+    const [enteredName, setEnteredName] = useState('');
+
+    const nameInputRef = useRef();
+
+    const handleNameInputChange = e => {
+        setEnteredName(e.target.value)
+    }
+
+    const handleFormSubmission = e => {
+        e.preventDefault();
+        console.log(enteredName)
+        const enteredRefName = nameInputRef.current.value;
+        console.log(enteredRefName)
+    }
+
+
     return (
-        <form>
+        <form onSubmit={handleFormSubmission}>
             <div className='form-control'>
                 <label htmlFor='name'>Your Name</label>
-                <input type='text' id='name' />
+                <input
+                    id='name'
+                    type='text'
+                    ref={nameInputRef}
+                    onChange={handleNameInputChange}
+                />
             </div>
             <div className="form-actions">
-                <button>Submit</button>
+                <button onClick={handleFormSubmission}>Submit</button>
             </div>
         </form>
     );
