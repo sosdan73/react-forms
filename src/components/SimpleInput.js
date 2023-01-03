fbe0b09d5bf5136d82481326b1ed5520531156fd
sosdan73 <daniel.sosinskiy@gmail.com>
2023-01-03 17:50:45 +0200

    adds validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,17 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 const SimpleInput = (props) => {
     const [enteredName, setEnteredName] = useState('');
+    const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+    const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
     const nameInputRef = useRef();
+
+    useEffect(() => {
+        if (enteredNameIsValid) {
+            console.log('name input is valid')
+        }
+    }, [enteredNameIsValid])
 
     const handleNameInputChange = e => {
         setEnteredName(e.target.value)
@@ -11,15 +19,27 @@ const SimpleInput = (props) => {
 
     const handleFormSubmission = e => {
         e.preventDefault();
+
+        setEnteredNameTouched(true);
+
+        if (enteredName.trim() === '') {
+            setEnteredNameIsValid(false)
+            return
+        }
+
+        setEnteredNameIsValid(true)
         console.log(enteredName)
-        const enteredRefName = nameInputRef.current.value;
-        console.log(enteredRefName)
     }
 
+    const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+    const nameInputClasses = nameInputIsInvalid
+        ? 'form-control invalid'
+        : 'form-control';
 
     return (
         <form onSubmit={handleFormSubmission}>
-            <div className='form-control'>
+            <div className={nameInputClasses}>
                 <label htmlFor='name'>Your Name</label>
                 <input
                     id='name'
@@ -27,6 +47,7 @@ const SimpleInput = (props) => {
                     ref={nameInputRef}
                     onChange={handleNameInputChange}
                 />
+                {nameInputIsInvalid && <p className="error-text">Name must not be empty</p>}
             </div>
             <div className="form-actions">
                 <button onClick={handleFormSubmission}>Submit</button>
